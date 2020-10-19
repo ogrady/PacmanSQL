7c67e940461d7d2e9825bd9f1c910a299622f911
Daniel O'Grady <danielogrady@gmx.de>
2020-10-19 07:51:14 +0200

    Add some preparations for frontend

---
+++ build/src/frontend.js
@@ -0,0 +1,17 @@
+"use strict";
+//import * as d from "dom";
+class Game {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+    }
+    render(canvas) {
+        var x = document;
+    }
+    drawBackground() {
+    }
+    drawMap() {
+    }
+    drawActors() {
+    }
+}
---
+++ src/frontend.ts
@@ -0,0 +1,27 @@
+//import * as d from "dom";
+
+class Game {
+	private width: number;
+	private height: number;
+
+    public constructor(w: number,h: number) {
+    	this.width = w;
+    	this.height = h;
+    }
+
+    public render(canvas: any): void {
+    	var x = document;
+    }
+
+    private drawBackground(): void {
+
+    }
+
+    private drawMap(): void {
+
+    }
+
+    private drawActors(): void {
+
+    }
+}
