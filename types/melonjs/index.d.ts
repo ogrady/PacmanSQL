05ced6aecf4d2ccf69d7eb9e0a386a8806ab4275
Daniel O'Grady <danielogrady@gmx.de>
2020-10-25 16:45:16 +0100

    Migrate from Grunt to WebPack for Typescript

--- types/melonjs/index.d.ts
+++ types/melonjs/index.d.ts
@@ -1,4 +1,20 @@
 declare module "melonjs" {
+    
+    export const audio: any;
+    export const debug: any;
+    export const device: any;
+    export const event: any;
+    export const game: any;
+    export const input: any;
+    export const loader: any;
+    export const plugin: any;
+    export const pool: any;
+    export const state: any;
+    export const video: any;
+
+    export class Stage {
+    }
+
     export class Renderable{
         public static extend(x: any): any;
     }
